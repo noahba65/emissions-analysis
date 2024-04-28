4a8b6df0ab02b07b3b476bdf0dd1561081e1faa2
Noah Anderson <noahanderson6556@gmail.com>
2024-04-28 17:54:05 -0500

    Created functions for calculating total ffi budget

--- carbon_budget.R
+++ carbon_budget.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(readxl)
 
-carbon_ffi_df <- read_csv("/Users/ayw1327/Documents/GitHub/emissions-analysis/Data/annual-co2-emissions-per-country.csv") %>%
+carbon_ffi_df <- read_csv("~/Documents/GitHub/emissions-analysis/Data/annual-co2-emissions-per-country.csv") %>%
   rename(annual_co2 = `Annual CO₂ emissions`)
 carbon_luc_df <- read_csv("~/Documents/GitHub/emissions-analysis/Data/co2-land-use.csv") %>%
   rename(annual_co2 = `Annual CO₂ emissions from land-use change`)
@@ -15,5 +15,10 @@ colnames(carbon_ffi_df) <- colnames(carbon_ffi_df) %>%
 colnames(carbon_luc_df) <- colnames(carbon_luc_df) %>%
   tolower() 
 
-ggplot(data = ffi_df %>% filter(entity == "United States")) +
+ggplot(data = carbon_ffi_df %>% filter(entity == "United States")) +
   geom_smooth(aes(x = year, y = annual_co2), method = "gam", color = "blue")
+
+
+total_q_luc <- sum(sapply(2012:2100, function(year) q_luc_time_function(year, start_year = 2012, 2100, carbon_luc_df
+                                                                        )))
+
--- functions.R
+++ functions.R
@@ -1,23 +1,44 @@
 
-world_past_emissions<- function(start_year, carbon_df){
+world_past_emissions <- function(start_year, carbon_df){
   carbon_df %>%
-    filter(year < start_year, entity == "World") %>%
+    filter(year <= start_year, entity == "World") %>%
     summarize(sum(annual_co2, na.rm = T))
 }
 
-# Q (t-T0)=-Q0(LUC)2100 - T0(t - T0)+ Q0(LUC)
 
-projected_q_luc <- function(year, q_luc_start, ){
-  
-}
-
-q_luc_time_function <- function(year, start_year, luc_df){
+q_luc_time_function <- function(year, start_year, end_year = 2100, luc_df){
   q_luc_start <- luc_df %>% 
-    filter(year == start_year) %>%
+    filter(year == start_year, entity == "World") %>%
     pull(annual_co2)
   
-  end_year <- 2100
+  q_luc_t <- q_luc_start * (1 - (year - start_year) / (end_year - start_year))
   
+  return(q_luc_t)
+}
+
+q_future_luc_function <- function(start_year, end_year = 2100, luc_df){
+  sum(sapply((start_year + 1):end_year, function(year) q_luc_time_function(year, start_year, end_year, luc_df)))
+}
+
+# Q_past(FFI) = world_past_emissions(start_year = 2012, ffi_df )
+# 
+# Q_past(LUC) = world_past_emissions(start_year = 2012, luc_df)
+# 
+# Q_future(LUC) = q_luc_future(start_year = 2012, luc_df)
+
+
+q_ffi_future_function <- function(start_year, q_tot, ffi_df, luc_df, end_year = 2100){
   
+  q_past_ffi <- world_past_emissions(start_year = start_year, end_year = end_year, 
+                                     carbon_df = ffi_df)
   
+  q_past_luc <- world_past_emissions(start_year = start_year, end_year = end_year,
+                                     carbon_df = luc_df)
+  
+  q_future_luc <- q_future_luc_function(start_year = start_year, end_year = end_year,
+                                        luc_df = luc_df) 
+  
+  q_ffi_future <- q_tot - q_past_ffi - q_past_luc - q_future_luc
+  
+  return(q_ffi_future)
 }
